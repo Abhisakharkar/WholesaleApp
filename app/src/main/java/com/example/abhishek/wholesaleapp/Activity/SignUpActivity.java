1dbe1397ac7798d68e771c94895e53e5d717054b
abhishek <deshpandeabhi.10@gmail.com>
2019-03-29 10:19:19 +0530

    ssl temporary implementation

--- app/src/main/java/com/example/abhishek/wholesaleapp/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/abhishek/wholesaleapp/Activity/SignUpActivity.java
@@ -5,23 +5,43 @@ import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BaseHttpStack;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.abhishek.wholesaleapp.Contract.SignUpContract;
 import com.example.abhishek.wholesaleapp.Presenter.SignUpPresenter;
 import com.example.abhishek.wholesaleapp.R;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.security.KeyManagementException;
 import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManagerFactory;
 
 public class SignUpActivity
@@ -62,7 +82,9 @@ public class SignUpActivity
     }
 
     public void SignUpButtonOnClick(View view) {
-        presenter.signUp(mailEdittext.getText(), passEdittext.getText(), confirmPassEdittext.getText());
+//        presenter.signUp(mailEdittext.getText(), passEdittext.getText(), confirmPassEdittext.getText());
+        sendSSLRequest();
+
     }
 
     //Contract Methods
@@ -83,7 +105,7 @@ public class SignUpActivity
     public void temp() {
         try {
             CertificateFactory cf = CertificateFactory.getInstance("X.509");
-            InputStream caInput = getResources().openRawResource(R.raw.MyRootCA);
+            InputStream caInput = getResources().openRawResource(R.raw.myrootca);
             Certificate ca;
             ca = cf.generateCertificate(caInput);
             caInput.close();
@@ -108,6 +130,91 @@ public class SignUpActivity
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendSSLRequest() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this, new HurlStack(null, getSocketFactory()));
+        String url = "https://ec2-13-234-45-216.ap-south-1.compute.amazonaws.com:443";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url
+                , new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("SSL Response", "onResponse: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley SSL Error", "onErrorResponse: " + error.toString());
+                Log.e("Volley SSL Error", "onErrorResponse: message : " + error.getMessage());
+            }
+        });
+        requestQueue.add(stringRequest);
+    }
+
+    private SSLSocketFactory getSocketFactory() {
+
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = getResources().openRawResource(R.raw.myrootca);
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(caInput);
+                Log.e("CERT", "ca=" + ((X509Certificate) ca).getSubjectDN());
+            } finally {
+                caInput.close();
+            }
+
+
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+
+            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+
+                    Log.e("CipherUsed", session.getCipherSuite());
+                    return hostname.compareTo("ubuntu") == 0; //The Hostname of your server
+
+                }
+            };
+
+
+//            HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);/
+            SSLContext context = null;
+            context = SSLContext.getInstance("TLS");
+
+            context.init(null, tmf.getTrustManagers(), null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+
+            SSLSocketFactory sf = context.getSocketFactory();
+
+
+            return sf;
+
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 }
